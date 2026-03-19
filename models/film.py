1f758bbe007924332a9241f3fd0618e3e9b76d98
nikaakor <151680050+nikaakor@users.noreply.github.com>
2026-03-19 14:27:09 +0200

    lab3

---
+++ models/film.py
@@ -0,0 +1,8 @@
+class Film:
+    def __init__(self, id, title, category, year, rating, image_url):
+        self.id = id
+        self.title = title
+        self.category = category
+        self.year = year
+        self.rating = rating
+        self.image_url = image_url 
